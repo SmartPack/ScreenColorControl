9573adbe42012f796daca61fdd2fc56070b86695
sunilpaulmathew <sunil.kde@gmail.com>
2020-02-27 21:50:50 +0100

    Main: Add a help image to guide for non-rooted devices
    Signed-off-by: sunilpaulmathew <sunil.kde@gmail.com>

--- app/src/main/java/com/smartpack/colorcontrol/MainActivity.java
+++ app/src/main/java/com/smartpack/colorcontrol/MainActivity.java
@@ -9,10 +9,12 @@
 package com.smartpack.colorcontrol;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageView;
 import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.tabs.TabLayout;
@@ -39,12 +41,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        AppCompatImageView unsupported = findViewById(R.id.no_root_Image);
         TextView textView = findViewById(R.id.no_root_Text);
         TabLayout tabLayout = findViewById(R.id.tabLayoutID);
         ViewPager viewPager = findViewById(R.id.viewPagerID);
 
         if (!RootUtils.rootAccess()) {
             textView.setText(getString(R.string.no_root));
+            unsupported.setImageDrawable(getResources().getDrawable(R.drawable.ic_help));
             return;
         }
 
@@ -57,6 +61,10 @@ public class MainActivity extends AppCompatActivity {
 
         viewPager.setAdapter(adapter);
         tabLayout.setupWithViewPager(viewPager);
+    }
+
+    public void androidRooting(View view) {
+        Utils.launchUrl("https://www.google.com/search?site=&source=hp&q=android+rooting+magisk", this);
     }
 
 }
